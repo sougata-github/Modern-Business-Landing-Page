f398429d39dba19606c6fbc8a8163faccb927336
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-08-27 01:05:13 +0530

    Add id.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,7 +2,7 @@ import { clients } from "../constants";
 import styles from "../style";
 
 const Clients = () => (
-  <section className={`${styles.flexCenter} my-4`}>
+  <section className={`${styles.flexCenter} my-4`} id="clients">
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
         <div
